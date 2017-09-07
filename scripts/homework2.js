93a4fe7f725edbdc90c5c67299ec131649c63f7d
Wingfly <tiko1581@mail.ru>
2017-09-08 00:01:38 +0400

    printing value finished

--- scripts/homework2.js
+++ scripts/homework2.js
@@ -1,10 +1,30 @@
+//menu item clicks
 const classname = document.getElementsByClassName("itemSpan");
-const myFunction = function() 
+const itemClick = function() 
 {
     alert("nothing for now");
 };
 
 for (var i = 0; i < classname.length; i++) 
 {
-    classname[i].addEventListener('click', myFunction, false);
+    classname[i].addEventListener('click', itemClick, false);
+}
+//calculator
+const printBtnClick = function ()
+{
+	const input = document.getElementById("numInput").value;
+	if (input === '')
+		alert("hey, type something!!");
+	else if (isNaN(parseInt(input)))
+		notifyUser(input);
+	else 
+		notifyUser(eval(input));
+};
+
+function notifyUser(input)
+{
+	const print = document.getElementsByClassName("print");
+	print[0].checked ? alert(input) : "" ;
+	print[1].checked ? document.write(input) : "" ;
+	print[2].checked ? console.log(input) : "" ;
 }
